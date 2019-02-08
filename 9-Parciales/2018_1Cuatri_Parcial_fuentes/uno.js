97a63f16862263b8ea65931703baed97f5d480d1
alumno <alumno>
2019-02-08 12:32:10 -0300

    termine el 1 del parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,21 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
+
+	ancho=prompt("Ingrese el ancho");
+	largo=prompt("Ingrese el largo");
+
+	ancho=parseInt(ancho);
+	largo=parseInt(largo);
+	perimetro=parseInt(perimetro);
+	
+	perimetro=(ancho+largo)*2;
+    //perimetro=parseint(perimetro);
+
+	alert("El perimetro es: "+perimetro);
+
 
 }
