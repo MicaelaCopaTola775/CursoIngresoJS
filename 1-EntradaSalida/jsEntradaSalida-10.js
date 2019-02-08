9529e9b230f80990032cebf9b78c060a39bd5e86
alumno <alumno>
2019-02-08 12:05:26 -0300

    termine el ejercicio 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,20 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var primerNumero;
+	var porcentaje;
+	var sueldoFinal;
+
+	primerNumero=importe.value
+	sueldoFinal=resultado.value
+
+	primerNumero= parseInt(primerNumero);
+	sueldoFinal= parseInt(sueldoFinal);
+
+	porcentaje= primerNumero*25/100;
+
+	sueldoFinal= primerNumero-porcentaje;
+
+	resultado.value=sueldoFinal
+
 }
