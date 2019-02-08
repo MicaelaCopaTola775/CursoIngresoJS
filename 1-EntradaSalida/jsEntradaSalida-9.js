24e9333574f88b8c6178a5300343b2487fa4ae47
alumno <alumno>
2019-02-08 12:00:55 -0300

    termine el ejercicio 9

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,22 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
-	
+	var primerNumero;
+	var porcentaje;
+	var sueldoFinal;
+
+	primerNumero=sueldo.value
+	sueldoFinal=resultado.value
+
+	primerNumero= parseInt(primerNumero);
+	sueldoFinal= parseInt(sueldoFinal);
+
+	porcentaje= primerNumero*10/100;
+
+	sueldoFinal= primerNumero+porcentaje;
+
+	resultado.value=sueldoFinal
+
+	//document.getElementById('resultado')=sueldoFinal
+
 }
