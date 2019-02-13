5d4ac60a80c4718cc2ffb019644e7c629545dc10
MicaelaCopaTola775 <47418401+MicaelaCopaTola775@users.noreply.github.com>
2019-02-12 22:16:04 -0300

    termine el 3 del parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,17 @@
 function mostrar()
 {
+	var precio;
+	var porcentaje;
+	var precioFinal;
 
+	precio=parseInt(precio);
+	porcentaje=parseInt(porcentaje);
+
+	precio=prompt("Ingrese el precio");
+	porcentaje=prompt("Ingrese el porcentaje");
+
+	descuento=precio*porcentaje/100;
+	precioFinal=precio-descuento;
+
+	document.getElementById('elPrecioFinal').value=precioFinal
 }
