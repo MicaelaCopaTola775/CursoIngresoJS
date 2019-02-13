eb03448a3937ab7b2f1522dbbb8ae98bec84449a
MicaelaCopaTola775 <47418401+MicaelaCopaTola775@users.noreply.github.com>
2019-02-12 22:28:54 -0300

    termine el 1 del recuperatorio

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
 
+	ancho=prompt("Ingrese el ancho");
+	largo=prompt("Ingrese el largo");
+
+	ancho=parseInt(ancho);
+	largo=parseInt(largo);
+
+	perimetro=(ancho+largo)*2;
+
+	alert("El perimetro es: "+perimetro)
 }
