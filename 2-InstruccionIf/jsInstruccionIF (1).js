51024125ccd1c9654e55373f25164bc1ed4ba6b2
alumno <alumno>
2019-02-14 09:49:15 -0300

    termine el 1 del if

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,12 @@
 function mostrar()
-{
-//tomo la edad  
+{	
+	var laedad;
 
-
+	laedad=edad.value;
+	laedad=parseInt(laedad);
+//tomo la edad
+	if(laedad==15){
+		alert ("Niña bonita");
+	}
 
 }//FIN DE LA FUNCIÓN
