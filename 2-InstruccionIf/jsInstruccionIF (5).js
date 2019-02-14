819ede3410d7d85b0a06301fc614906a797e1368
alumno <alumno>
2019-02-14 10:28:32 -0300

    termine el 5 del if

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,12 @@
 function mostrar()
 {
-//tomo la edad  
+	var laEdad;
 
+	laEdad=document.getElementById('edad').value;
+	laEdad=parseInt(laEdad);
+//tomo la edad  
+	if (laEdad<13||laEdad>17) {
+		alert("Usted no es adolescente")
+	}
 
 }//FIN DE LA FUNCIÓN
