dcf8cfa27ecb611e1a96ea2298b0ea8aedb2d7e1
alumno <alumno>
2019-02-14 10:02:32 -0300

    termine el 3 del if

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,14 @@
 function mostrar()
 {
-//tomo la edad  
+	var laEdad;
 
+	laEdad=edad.value;
+	laEdad=parseInt(laEdad);
+//tomo la edad  
+	if (laEdad>=18) {
+		alert("Usted es mayor de edad")
+	} else {
+		alert("Usted es menor de edad")
+	}
 
 }//FIN DE LA FUNCIÓN
