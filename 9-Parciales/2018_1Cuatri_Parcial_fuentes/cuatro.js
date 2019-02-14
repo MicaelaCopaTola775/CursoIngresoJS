e2ad70dff1f24100a1d08b54943c0d5aafbe1df6
alumno <alumno>
2019-02-14 12:18:41 -0300

    haciendo el 4 del parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,13 @@
 function mostrar()
 {
+	var primerNumero;
+	var segundoNumero;
 
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+
+	prompt("Ingrese el primer numero")=primerNumero;
+	prompt("Ingrese el segundo numero")=segundoNumero;
+
+	
 }
