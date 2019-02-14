1b753a5c175b40e50b9838bf2edfd2f3917b0fc5
alumno <alumno>
2019-02-14 11:14:07 -0300

    termine el 8 del if

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,6 +1,18 @@
 function mostrar()
-{
+{	
+	var laEdad;
+	var suEstadoCivil;
+
+	laEdad=document.getElementById('edad').value;
+	suEstadoCivil=document.getElementById('estadoCivil').value
+
+	laEdad=parseInt(laEdad);
 //tomo la edad  
+	if (laEdad<18&&suEstadoCivil!="Soltero") {
+
+	} if (suEstadoCivil="Soltero"&&laEdad>=18) {
+		alert("Es soltero y no es menor de edad")
+	} else {}
 
 	
 
