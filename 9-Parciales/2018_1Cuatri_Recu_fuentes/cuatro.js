3be5ed6fc013ba33267031d7b3c0a2c9953668c2
alumno <alumno>
2019-02-15 11:13:45 -0300

    haciendo el ejercicio 4

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,17 @@
 function mostrar()
 {
+	libros=prompt("Ingrese la cantidad de libros");
+	precio=prompt("Ingrese el precio de cada libro");
 
+	precio=parseInt(precio);
+	libros=parseInt(libros);
+
+	compra=precio*libros
+
+	if (libros>2) 
+	{
+		porsentajeDes=compra
+	} else {
+
+	}
 }
