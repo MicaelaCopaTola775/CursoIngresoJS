ecaa6b17a6de09307b125a96b6b6a18ff08a2b95
alumno <alumno>
2019-02-15 11:50:58 -0300

    haciendo 10 del if

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,15 @@
 function mostrar()
-{
+{	
+	var nota;
+
+	nota=Math.floor((Math.random() * 10) + 1);
 	//Genero el número RANDOM entre 1 y 10 
-	
+	if (nota=="9"&&nota=="10") {
+		alert("Excelente")
+} else {
+	if (nota>4) {
+		alert("")
+	} else {}
+}
 
 }//FIN DE LA FUNCIÓN
