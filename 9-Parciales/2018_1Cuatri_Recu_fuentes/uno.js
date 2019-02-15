6554d263cec4cfeb26f5faee077245d1ed13d08e
alumno <alumno>
2019-02-15 09:52:38 -0300

    ejercicio 1

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,17 +1,22 @@
 
 function mostrar()
 {
-	var ancho;
-	var largo;
-	var perimetro;
+	var mascota1;
+	var mascota2;
+	var kilo1;
+	var kilo2;
+	var suma;
+ 
+ 	mascota1=prompt("Ingrese el nombre de su primer mascota");
+ 	mascota2=prompt("Ingrese el nombre de su segunda mascota");
 
-	ancho=prompt("Ingrese el ancho");
-	largo=prompt("Ingrese el largo");
+	kilo1=prompt("Ingrese el kilo de su primer mascota");
+	kilo2=prompt("Ingrese el kilo de su segunda mascota");
 
-	ancho=parseInt(ancho);
-	largo=parseInt(largo);
+	kilo1=parseInt(kilo1);
+	kilo2=parseInt(kilo2);
+ 
+	suma=kilo1+kilo2
 
-	perimetro=(ancho+largo)*2;
-
-	alert("El perimetro es: "+perimetro)
+	alert("Tenes dos mascotas: "+mascota1+" y "+mascota2+",que pesan: "+kilo1+" y "+kilo2+", la suma de los kilos es "+suma)
 }
