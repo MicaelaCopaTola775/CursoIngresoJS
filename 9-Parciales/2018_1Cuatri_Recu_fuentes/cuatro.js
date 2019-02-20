5c25753664796c768ce8afd8b06b5c8c772a504d
MicaelaCopaTola775 <47418401+MicaelaCopaTola775@users.noreply.github.com>
2019-02-20 20:34:29 -0300

    terminando el 4 del recu

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,17 +1,47 @@
 function mostrar()
 {
+	var precio;
+	var libros;
+	var porcentajeDes;
+	var descuento;
+	var porcentajeTar;
+	var recargo;
+
 	libros=prompt("Ingrese la cantidad de libros");
 	precio=prompt("Ingrese el precio de cada libro");
+	pago=prompt("¿Usted pagara con tarjeta o en efectivo");
 
 	precio=parseInt(precio);
 	libros=parseInt(libros);
+	porcentajeDes=parseInt(porcentajeDes);
+	descuento=parseInt(descuento);
+	porcentajeTar=parseInt(porcentajeTar);
+	recargo=parseInt(recargo);
 
 	compra=precio*libros
 
 	if (libros>2) 
 	{
-		porsentajeDes=compra
-	} else {
+		porcentajeDes=compra*10/100;
+		descuento=compra-porcentajeDes;
 
-	}
+	} else 
+	{
+		if (compra>2000) 
+			{
+				porcentajeDes=compra*15/100;
+				descuento=compra-porcentajeDes;
+			} else 
+			{
+				if (pago=="tarjeta") 
+				{
+					porcentajeTar=descuento*10/100;
+					recargo=descuento+porcentajeTar;
+				}
+
+			}			
+ 	alert("Usted compró "+libros+" libros y el precio de cada libro es de $"+precio+", el total a pagar es de $"+compra+". Al hacerle el descuento su total quedo en $"+descuento)
 }
+
+}
+
