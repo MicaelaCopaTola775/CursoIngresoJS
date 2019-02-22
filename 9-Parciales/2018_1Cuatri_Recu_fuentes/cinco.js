fdec1d5fb6b973e92fea680ee9755dbf25c172ba
MicaelaCopaTola775 <47418401+MicaelaCopaTola775@users.noreply.github.com>
2019-02-22 11:43:51 -0300

    tp en clase

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,112 @@
 function mostrar()
 {
+	var importe1;
+	var importe2;
+	var importe3;
+	var importe4;
 
+	importe1=prompt("Ingrese el primer importe: ");
+	importe2=prompt("Ingrese el segundo importe");
+	importe3=prompt("Ingrese el tercer importe");
+	importe4=prompt("Ingrese el cuarto importe");
+
+	importe1=parseInt(importe1);
+	importe2=parseInt(importe2);
+	importe3=parseInt(importe3);
+	importe4=parseInt(importe4);
+
+	if (importe1>100) 
+		{
+			descuento=importe1*0.10;
+			total1=importe1-descuento;
+		} else 
+		{
+			if (importe1>50) 
+				{
+					descuento=importe1*0.5;
+					total1=importe1-descuento;
+				} else 
+				{
+					descuento=importe1*0.15;
+					total1=importe1+descuento;
+				}
+		}
+	if (importe2>100) 
+		{
+			descuento2=importe2*0.10;
+			total2=importe2-descuento2;
+		} else 
+		{
+			if (importe2>50) 
+				{
+					descuento2=importe2*0.5;
+					total2=importe2-descuento2;
+				} else 
+				{
+					descuento2=importe2*0.15;
+					total2=importe2+descuento2;
+				}
+		}
+
+	if (importe3>100) 
+		{
+			descuento3=importe3*0.10;
+			total3=importe3-descuento3;
+		} else 
+		{
+			if (importe3>50) 
+				{
+					descuento3=importe3*0.5;
+					total3=importe3-descuento3;
+				} else 
+				{
+					descuento3=importe3*0.15;
+					total3=importe3+descuento3;
+				}
+		}
+
+	if (importe4>100) 
+		{
+			descuento4=importe4*0.10;
+			total4=importe4-descuento4;
+		} else 
+		{
+			if (importe4>50) 
+				{
+					descuento4=importe4*0.5;
+					total4=importe4-descuento4;
+				} else 
+				{
+					descuento4=importe4*0.15;
+					total4=importe4+descuento4;
+				}
+		}
+	/*
+	switch(importe1)
+	{
+		case >100:
+		descuento=importe1*0.10;
+		break
+		case >50:
+		descuento=importe1*0.5;
+		break
+		case <50;
+		descuento=importe1*0.15;
+		break
+	}
+
+	switch(importe2)
+	{
+		case >100:
+		descuento2=importe2*0.10;
+		break
+		case >50:
+		descuento2=importe2*0.5;
+		break
+		case <50:
+		descuento2=importe3*0.15;
+		break
+	}
+	*/
+	alert("Importe 1: "+total1+", importe 2: "+total2+", importe 3: "+total3+", importe 4: "+total4);
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/seis.js
@@ -1,4 +1,50 @@
 function mostrar()
 {
+	var pago;
+	var paquete;
 
+	compra=prompt("Ingrese el precio de la compra");
+	pago=prompt("Ingrese el tipo de pago: tarjeta visa, paypal, mercado pago, efectivo u otro");
+	paquete=prompt("Ingrese el tipo de paquete: todo incluido, solo desayunos u otro");
+
+	compra=parseInt(compra);
+
+	switch(pago)
+	{
+		case "tarjeta visa":
+		case "mercado pago":
+			total=compra-(compra*0.10);
+		break
+		case "paypal":
+			total=compra-(compra*0.15);
+			switch(paquete)
+			{	
+				case "todo incluido":
+					aumento=total+(total*0.10);
+				break
+			}
+		break
+		case "efectivo":
+			total=compra-(compra*0.20);
+			switch(paquete)
+			{
+				case "solo desayunos":
+					aumento=total+(total*0.10);
+				break
+				case "todo incluido":
+					aumento=total+(total*0.15);
+				break
+				default
+					aumento="No tiene descuento adicional";
+				break
+			}
+		break
+		default
+			total=compra-(compra*0.5);
+		break
+
+	}
+
+	alert("Compra: "+compra+", Tipo de pago: "+pago+", Tipo de paquete: "+paquete+", Descuento: "+total+", Aumento: "+aumento);
+    
 }
