b8ff6f238601dd84b861826d8d2c29554ed8d352
MicaelaCopaTola775 <47418401+MicaelaCopaTola775@users.noreply.github.com>
2019-02-22 12:22:30 -0300

    ejercicios while 1-4

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -2,6 +2,16 @@ function mostrar()
 {
 
 var clave = prompt("ingrese el número clave.");
+var cont=0;
 
-
+while(clave!="utn750")
+{
+	clave=prompt("Ingrese el número clave.");
+	//cont++;
+	console.log(cont++);
+	if (cont>3)
+	 {
+	 	break 
+	 }
+}
 }//FIN DE LA FUNCIÓN
