0895dc0da4879057d9e331661a43cb04b8cbfd66
MicaelaCopaTola775 <47418401+MicaelaCopaTola775@users.noreply.github.com>
2019-02-28 12:18:48 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,30 @@
 function mostrar()
 {
+	var cont=0;
+	var sexo;
+	var nota;
+	var max;
 
+
+	while(cont<5)
+	{
+		cont++;
+		do
+		{
+			sexo=prompt("Ingrese su sexo");
+		} while(sexo!="m"&&sexo!="f");
+		do
+		{
+			nota=prompt("Ingrese su nota");
+			nota=parseInt(nota);
+		} while(esNaN(nota)&&(nota<0&&nota>10));
+		
+		promedio=nota/cont;
+
+		if(nota>max)
+		{
+
+		}
+		
+	}
 }
