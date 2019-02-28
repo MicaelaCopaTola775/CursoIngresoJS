d4f696b4e744a40185e3c3ed15c8c2228ea11cc4
MicaelaCopaTola775 <47418401+MicaelaCopaTola775@users.noreply.github.com>
2019-02-28 19:00:02 -0300

    while 5

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -2,9 +2,19 @@ function mostrar()
 {
 
 var sexo = prompt("ingrese f ó m .");
-
-
+var cont=0;
 
 document.getElementById('Sexo').value=sexo;
 
+while(sexo!="f"&&sexo!="m")
+{
+	sexo = prompt("Ingrese f ó m .");
+	cont++;
+
+	if (cont<3) 
+	{
+		break
+	}
+}
+//document.getElementById('Sexo').value=sexo;
 }//FIN DE LA FUNCIÓN
